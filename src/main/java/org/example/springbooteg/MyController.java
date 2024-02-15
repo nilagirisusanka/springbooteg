56d3e26c3fee920c34f34f0969987b614bc19a8d
nilairisusanka <nilagirisusanka@gmail.com>
2024-02-15 09:45:13 +0530

    added comments for endpoints

--- src/main/java/org/example/springbooteg/MyController.java
+++ src/main/java/org/example/springbooteg/MyController.java
@@ -7,10 +7,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyController {
 
         @GetMapping("/hello")
+        //this is my endpoint hello
         public String helloeveryone(){
             return "Hello everyone, Welcome to Spring Boot and happy learning";
         }
-
+    @GetMapping("/hai")
+    //this is end point hai
+    public String hai(){
+        return "Have a nice day";
+    }
 
 }
 
